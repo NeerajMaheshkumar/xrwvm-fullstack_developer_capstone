7e37c24eff11672bec577c0536729fd2142800fb
Neeraj Maheshkumar <neerajmaheshkumar3@gmail.com>
2026-01-14 05:30:14 -0500

    registration

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,11 +1,13 @@
 import LoginPanel from "./components/Login/Login"
 import { Routes, Route } from "react-router-dom";
+import Registration from "./components/Register/Register" 
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
+    <Route path="/registration" element={<Registration />} />
     </Routes>
-  );
+  );  
 }
 export default App;
